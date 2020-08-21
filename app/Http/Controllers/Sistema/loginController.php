ddc4713989f3ad37871cb1f934c02cf874cf3827
Nakama-YefrixD <43789916+Nakama-YefrixD@users.noreply.github.com>
2020-08-21 12:49:36 -0500

    login correcto

--- app/Http/Controllers/Sistema/loginController.php
+++ app/Http/Controllers/Sistema/loginController.php
@@ -22,46 +22,45 @@ class loginController extends Controller
         $usuario    = $request['usuario'];
         $contrasena = $request['contrasena'];
 
-        echo $usuario;
-        // try{
+        try{
             
-        //     $usuusaurio = usuusuarios::where('usuusuario', $usuario)
-        //                                 ->first(['usucontrasena']);
+            $usuusaurio = usuusuarios::where('usuusuario', $usuario)
+                                        ->first(['usucontrasena']);
 
-        //     if($usuusaurio){
+            if($usuusaurio){
 
-        //         if (Hash::check($contrasena, $usuarusuusaurioio->usucontrasena)) {
-        //             $respuesta      = true;
-        //             $mensaje        = 'Login Correcto';
-        //             $mensajeDetalle = 'Bienvenido.';
-        //             $linea          = __LINE__;
-        //         }else{
-        //             $respuesta      = false;
-        //             $mensaje        = 'Login Incorrecto';
-        //             $mensajeDetalle = 'La contraseña es incorrecta.';
-        //             $linea          = __LINE__;
-        //         }
+                if (Hash::check($contrasena, $usuarusuusaurioio->usucontrasena)) {
+                    $respuesta      = true;
+                    $mensaje        = 'Login Correcto';
+                    $mensajeDetalle = 'Bienvenido.';
+                    $linea          = __LINE__;
+                }else{
+                    $respuesta      = false;
+                    $mensaje        = 'Login Incorrecto';
+                    $mensajeDetalle = 'La contraseña es incorrecta.';
+                    $linea          = __LINE__;
+                }
 
-        //     }else{
-        //         $respuesta      = false;
-        //         $mensaje        = 'Login Incorrecto';
-        //         $mensajeDetalle = 'El usuario no esta registrado.';
-        //         $linea          = __LINE__;
+            }else{
+                $respuesta      = false;
+                $mensaje        = 'Login Incorrecto';
+                $mensajeDetalle = 'El usuario no esta registrado.';
+                $linea          = __LINE__;
 
-        //     }
+            }
 
-        // } catch (Exception $e) {
-        //     $mensajedev = $e->getMessage();
-        //     $linea      = __LINE__;
-        // }
+        } catch (Exception $e) {
+            $mensajedev = $e->getMessage();
+            $linea      = __LINE__;
+        }
         
-        // return response()->json([
-        //     $respuesta      => $respuesta,
-        //     $mensaje        => $mensaje,
-        //     $datos          => $datos,
-        //     $linea          => $linea,
-        //     $mensajeDetalle => $mensajeDetalle,
-        //     $mensajedev     => $mensajedev
-        // ]);
+        return response()->json([
+            $respuesta      => $respuesta,
+            $mensaje        => $mensaje,
+            $datos          => $datos,
+            $linea          => $linea,
+            $mensajeDetalle => $mensajeDetalle,
+            $mensajedev     => $mensajedev
+        ]);
     }
 }
