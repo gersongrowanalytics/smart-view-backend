cce6dbfc2d19524cd48a0fa140651389a4d468e7
Nakama-YefrixD <43789916+Nakama-YefrixD@users.noreply.github.com>
2020-08-30 19:35:46 -0500

    modelo suc sucursales

---
+++ app/sucsucursales.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class sucsucursales extends Model
+{
+    protected $table = 'sucsucursales';
+    protected $primaryKey = 'sucid';
+}
