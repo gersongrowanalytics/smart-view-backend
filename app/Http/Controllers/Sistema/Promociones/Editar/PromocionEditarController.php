f899bed09cbc9f690192fd4c0d7e20357e3be0a3
Nakama-YefrixD <43789916+Nakama-YefrixD@users.noreply.github.com>
2020-09-01 13:16:19 -0500

    nueva imagen login

---
+++ app/Http/Controllers/Sistema/Promociones/Editar/PromocionEditarController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Sistema\Promociones\Editar;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PromocionEditarController extends Controller
+{
+    public function editarPromocion(Request $request)
+    {
+
+    }
+}
