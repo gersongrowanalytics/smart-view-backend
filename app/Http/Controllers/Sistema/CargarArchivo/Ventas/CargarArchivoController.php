16c74534774821a86a055826c293fc9c821cefc4
YefrixD <gerson.vilca@tecsup.edu.pe>
2020-09-14 14:56:31 -0500

    rtp

--- app/Http/Controllers/Sistema/CargarArchivo/Ventas/CargarArchivoController.php
+++ app/Http/Controllers/Sistema/CargarArchivo/Ventas/CargarArchivoController.php
@@ -190,7 +190,8 @@ class CargarArchivoController extends Controller
                         if($tsu){
                             $tsuid = $tsu->tsuid;
                             $nuevoReal = $tsu->tsuvalorizadoreal+$real;
-                            $porcentajeCumplimiento = ($nuevoReal/$tsuvalorizadoobjetivo)-1;
+                            $porcentajeCumplimiento = ($nuevoReal/$tsu->$tsuvalorizadoobjetivo)-1;
+                            echo $porcentajeCumplimiento;
                             // OBTENER INFORMACION DEL REBATE
                             $rtp = rtprebatetipospromociones::where('fecid', $fecid)
                                                             ->where('tprid', 1) // TIPO DE PROMOCION SELL IN
