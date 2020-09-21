246749115b4f5aab619900b867fe83e89b2a347e
YefrixD <gerson.vilca@tecsup.edu.pe>
2020-09-21 12:58:32 -0500

    obtener data para descargar excel

--- app/Http/Controllers/Sistema/Promociones/Mostrar/CategoriasPromocionesMostrarController.php
+++ app/Http/Controllers/Sistema/Promociones/Mostrar/CategoriasPromocionesMostrarController.php
@@ -394,7 +394,7 @@ class CategoriasPromocionesMostrarController extends Controller
             $numRows        = $objPHPExcel->setActiveSheetIndex(0)->getHighestRow();
             $ultimaColumna  = $objPHPExcel->setActiveSheetIndex(0)->getHighestColumn();
 
-            for ($i=2; $i <= 550 ; $i++) {
+            for ($i=2; $i <= 650 ; $i++) {
 
                 if($i == 2){
 
