20f509d043c8a3b50a44b083d07ea0e5ca62edaa
YefrixD <gerson.vilca@tecsup.edu.pe>
2020-09-23 18:26:03 -0500

    tipos de rebeta

--- app/Http/Controllers/Sistema/Configuracion/Rebate/Mostrar/RebateMostrarController.php
+++ app/Http/Controllers/Sistema/Configuracion/Rebate/Mostrar/RebateMostrarController.php
@@ -24,7 +24,7 @@ class RebateMostrarController extends Controller
         try{
             
             $rtp = rtprebatetipospromociones::join('trrtiposrebatesrebates as trr', 'trr.rtpid', 'rtprebatetipospromociones.rtpid')
-                                            ->join('tretiposrebates as tre', 'tre.treid', 'trr.treid')
+                                            // ->join('tretiposrebates as tre', 'tre.treid', 'trr.treid')
                                             ->join('fecfechas as fec', 'fec.fecid', 'rtprebatetipospromociones.fecid')
                                             ->join('tprtipospromociones as tpr', 'tpr.tprid', 'rtprebatetipospromociones.tprid')
                                             ->get([
@@ -36,7 +36,7 @@ class RebateMostrarController extends Controller
                                                 'fec.fecano',
                                                 'tpr.tprid',
                                                 'tpr.tprnombre',
-                                                'tre.trenombre',
+                                                // 'tre.trenombre',
                                                 'rtpporcentajedesde',
                                                 'rtpporcentajehasta',
                                                 'rtpporcentajerebate'
