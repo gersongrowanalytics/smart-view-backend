668929762f0781c583abcee6becb6812a06975c5
YefrixD <gerson.vilca@tecsup.edu.pe>
2020-09-28 13:33:37 -0500

    mostrar permisos del tipo de usuarios seleccionado

---
+++ app/Http/Controllers/Sistema/Configuracion/Usuarios/Mostrar/PermisosController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\Sistema\Configuracion\Usuarios\Mostrar;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\tuptiposusuariospermisos;
+use App\pempermisos;
+
+class PermisosController extends Controller
+{
+    public function MostrarPermisosTipoUsuario(Request $request) //MOSTRAR PERMISOS DE UN TIPO DE USUARIO
+    {
+
+        $tpuid = $request['tpuid'];
+
+        $tups = tuptiposusuariospermisos::join('pempermisos as pem', 'pem.pemid', 'tuptiposusuariospermisos.pemid')
+                                        ->where()
+                                        ->get([
+                                            'pem.pemid',
+                                            'pem.pemnombre',
+                                            'pem.pemslug',
+                                            'pem.pemruta'
+                                        ]);
+        
+        
+        $pems = pempermisos::get(['pemid', 'pemnombre', 'pemslug', 'pemruta']);
+
+        foreach($pems as $posicionPem => $pem){
+            foreach($tups as $tup){
+                if($pem->pemid == $tup->pemid){
+                    $pems[$posicionPem]['seleccionado'] = true;
+                    break;
+                }else{
+                    $pems[$posicionPem]['seleccionado'] = false;
+                }
+            }
+        }
+
+        $requestsalida = response()->json([
+            "respuesta" => true,
+            "mensaje"   => 'Los permisos se cargaron satisfactoriamente',
+            "datos"     => $pems
+        ]);
+        
+        return $requestsalida;
+    }
+}
