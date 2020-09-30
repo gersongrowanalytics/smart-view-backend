e5858a2641b56ecb9b1cbee9e5ad983496f5e9f1
YefrixD <gerson.vilca@tecsup.edu.pe>
2020-09-30 13:03:16 -0500

    mostrar todoas las sucursales con sus zonas respectivas anadido en permisos de tup al momento de mostrar sucursales

--- app/Http/Controllers/Sistema/Usuario/Sucursales/Mostrar/SucursalesMostrarController.php
+++ app/Http/Controllers/Sistema/Usuario/Sucursales/Mostrar/SucursalesMostrarController.php
@@ -29,12 +29,13 @@ class SucursalesMostrarController extends Controller
             if($usuusuario){
                 $tup = tuptiposusuariospermisos::join('pempermisos as pem', 'pem.pemid', 'tuptiposusuariospermisos.pemid')
                                                 ->where('pem.pemslug', 'mostrar.sucursales.zona.todo')
+                                                ->where('tuptiposusuariospermisos.tupid', $usuusuario->tpuid)
                                                 ->first([
                                                     'pem.pemid'
                                                 ]);
                                                 
                 // if($usuusuario->tpuid == 1){
-                if($tup || $usuusuario->tpuid == 1){
+                if($tup ){
 
                     $zonas = ussusuariossucursales::join('sucsucursales as suc', 'suc.sucid', 'ussusuariossucursales.sucid')
                                                     ->join('usuusuarios as usu', 'usu.usuid', 'ussusuariossucursales.usuid')
