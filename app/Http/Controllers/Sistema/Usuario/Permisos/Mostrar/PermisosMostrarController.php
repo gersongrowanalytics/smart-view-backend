517edf347fe6cf3bb3f0b8806547ef9609ffb830
YefrixD <gerson.vilca@tecsup.edu.pe>
2020-10-01 17:08:01 -0500

    namespace mostrar

--- app/Http/Controllers/Sistema/Usuario/Permisos/Mostrar/PermisosMostrarController.php
+++ app/Http/Controllers/Sistema/Usuario/Permisos/Mostrar/PermisosMostrarController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Sistema\Usuario\Permisos\Mostar;
+namespace App\Http\Controllers\Sistema\Usuario\Permisos\Mostrar;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
