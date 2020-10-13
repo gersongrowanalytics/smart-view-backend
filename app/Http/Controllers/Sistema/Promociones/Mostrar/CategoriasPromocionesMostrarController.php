38ad478da4c9f26e773eba2f55ce24b41c4b0d5e
YefrixD <gerson.vilca@tecsup.edu.pe>
2020-10-13 10:23:37 -0500

    descargaer promociones

--- app/Http/Controllers/Sistema/Promociones/Mostrar/CategoriasPromocionesMostrarController.php
+++ app/Http/Controllers/Sistema/Promociones/Mostrar/CategoriasPromocionesMostrarController.php
@@ -386,7 +386,8 @@ class CategoriasPromocionesMostrarController extends Controller
                             ->first(['fecid']);
 
             if($fec){
-                $car = carcargasarchivos::where('fecid', 2)
+                echo $fec->fecid;
+                $car = carcargasarchivos::where('fecid', $fec->fecid)
                                     ->where('tcaid', 1)
                                     ->first(['carid', 'carnombrearchivo']);
 
