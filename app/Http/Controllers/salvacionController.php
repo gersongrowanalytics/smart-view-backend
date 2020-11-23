a5c4460ae49d3e56d7846724ec29b6f6916a4177
unknown <gerson.vilca@grow-analytics.com>
2020-11-23 10:01:18 -0500

    salvacion

--- app/Http/Controllers/salvacionController.php
+++ app/Http/Controllers/salvacionController.php
@@ -15,9 +15,15 @@ class salvacionController extends Controller
             $proe = proproductos::find($pro->proid);
             // http://backs.gavsistemas.com/Sistema/promociones/IMAGENES/PRODUCTOS/7-493-1253-HTP%20talla%20M-31%20a%20mas%20planchas.png
             $ruta = explode("http://backs.gavsistemas.com/", $pro->proimagen);
-            // $ruta = $ruta[1];
-            $proe->proimagen = "http://backend.leadsmartview.com/".$ruta[1];
-            $proe->update();
+
+            if(sizeof($ruta) > 0){
+                $proe->proimagen = "http://backend.leadsmartview.com/".$ruta[1];
+                $proe->update();
+            }else{
+                echo "<br>no tiene: ".$pro->proid.' y nombre: '.$pro->proimagen.'<br>';
+
+            }
+            
 
         }
     }
