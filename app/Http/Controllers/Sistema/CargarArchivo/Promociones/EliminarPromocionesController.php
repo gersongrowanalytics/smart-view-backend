f0cc8ee4a83deb44e569857aa055964c0266c4a2
gersongrowanalytics <gerson.vilca@grow-analytics.com>
2020-12-17 12:30:48 -0500

    use app

--- app/Http/Controllers/Sistema/CargarArchivo/Promociones/EliminarPromocionesController.php
+++ app/Http/Controllers/Sistema/CargarArchivo/Promociones/EliminarPromocionesController.php
@@ -5,6 +5,30 @@ namespace App\Http\Controllers\Sistema\CargarArchivo\Promociones;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Http\Controllers\AuditoriaController;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use Illuminate\Support\Str;
+use App\fecfechas;
+use App\tputiposusuarios;
+use App\perpersonas;
+use App\usuusuarios;
+use App\catcategorias;
+use App\proproductos;
+use App\tprtipospromociones;
+use App\cancanales;
+use App\ussusuariossucursales;
+use App\sucsucursales;
+use App\scasucursalescategorias;
+use App\csccanalessucursalescategorias;
+use App\prmpromociones;
+use App\cspcanalessucursalespromociones;
+use App\prbpromocionesbonificaciones;
+use App\prppromocionesproductos;
+use App\carcargasarchivos;
+use App\tuptiposusuariospermisos;
+
 class EliminarPromocionesController extends Controller
 {
     public function CargarArchivoEliminarPromociones(Request $request)
