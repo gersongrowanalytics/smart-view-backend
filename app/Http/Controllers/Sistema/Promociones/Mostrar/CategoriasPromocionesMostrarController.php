de389883f7db11c85e7dc4ff23821131f50b87b8
gersongrowanalytics <gerson.vilca@grow-analytics.com>
2021-01-14 09:00:05 -0500

    order sucnombre

--- app/Http/Controllers/Sistema/Promociones/Mostrar/CategoriasPromocionesMostrarController.php
+++ app/Http/Controllers/Sistema/Promociones/Mostrar/CategoriasPromocionesMostrarController.php
@@ -386,7 +386,7 @@ class CategoriasPromocionesMostrarController extends Controller
                             ->first(['fecid']);
 
             if($fec){
-                $car = carcargasarchivos::where('fecid', $fec->fecid)
+                $car = carcargasarchivos::where('fecid', 12)
                                     ->where('tcaid', 1)
                                     ->first(['carid', 'carnombrearchivo']);
 
