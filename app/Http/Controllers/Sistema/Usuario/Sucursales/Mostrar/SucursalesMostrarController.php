3c4f8317749ff2a770a80dda379e713a3c9d2783
gersongrowanalytics <gerson.vilca@grow-analytics.com>
2021-01-18 15:47:41 -0500

    mostrar uss en el filtro

--- app/Http/Controllers/Sistema/Usuario/Sucursales/Mostrar/SucursalesMostrarController.php
+++ app/Http/Controllers/Sistema/Usuario/Sucursales/Mostrar/SucursalesMostrarController.php
@@ -92,7 +92,8 @@ class SucursalesMostrarController extends Controller
                                                     'gsuid',
                                                     'suc.casid',
                                                     'zon.zonnombre',
-                                                    'suc.sucnombre'
+                                                    'suc.sucnombre',
+                                                    'usu.ususoldto'
                                                 ]);
 
 
