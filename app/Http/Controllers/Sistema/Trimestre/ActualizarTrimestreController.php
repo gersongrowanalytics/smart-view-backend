bd6c762295ac84f3c5085f58de9d11bfa59f5fdf
gersongrowanalytics <gerson.vilca@grow-analytics.com>
2021-02-11 13:16:55 -0500

    actualizar trimestre extras de lo ques no entran

--- app/Http/Controllers/Sistema/Trimestre/ActualizarTrimestreController.php
+++ app/Http/Controllers/Sistema/Trimestre/ActualizarTrimestreController.php
@@ -173,6 +173,7 @@ class ActualizarTrimestreController extends Controller
                                     }
                                 }else{
                                     $logs[$tri->fecid]["noentra"][] = "No entra en la escala rebate: ".$tsue->tsuid." de la sucursal: ".$suc->sucid." con un cumplimiento de: ".round($tsucumplimientotrimestral)."";
+                                    $logs[$tri->fecid]["extras_noentra"][] = "fecid: ". $tri->fecid." triid: ".$tri->triid." treid: ".$suc->treid." ttrporcentajedesde <= ".round($tsucumplimientotrimestral)." ttrporcentajehasta >= ".round($tsucumplimientotrimestral);
                                 }
     
                                 $tsuu = tsutipospromocionessucursales::find($tsue->tsuid);
