9ab4fa1e6218ebd9185c94850fbd0d121f30b0da
gersongrowanalytics <gerson.vilca@grow-analytics.com>
2021-02-16 17:46:07 -0500

    CONTROL DE VENTAS X GRAFICO Y FILTROS

--- app/Http/Controllers/Sistema/Modulos/ControlVentas/VentasXZonasController.php
+++ app/Http/Controllers/Sistema/Modulos/ControlVentas/VentasXZonasController.php
@@ -100,7 +100,7 @@ class VentasXZonasController extends Controller
 
         $respuesta = true;
         $mensaje = "";
-        
+
         $datos = array(
             array(
                 "titulo"   => "",
@@ -416,28 +416,32 @@ class VentasXZonasController extends Controller
                 $sumReal = scasucursalescategorias::join('fecfechas as fec', 'fec.fecid', 'scasucursalescategorias.fecid')
                                                     ->join('sucsucursales as suc', 'suc.sucid', 'scasucursalescategorias.sucid')
                                                     ->join('tsutipospromocionessucursales as tsu', 'tsu.tsuid', 'scasucursalescategorias.tsuid')
-                                                    ->where('catid', $cat->catid)
-                                                    ->where('tsu.tprid', $tprid)
                                                     ->where(function ($query) use($anios, $meses, $regiones, $zonas, $grupos) {
-                                                                
+     
                                                         foreach($anios as $anio){
-                                                            $query->orwhere('fecano', $anio);
-                                                        }
-
-                                                        foreach($meses as $mes){
-                                                            $query->orwhere('fecmes', $mes);
-                                                        }
-                                                        
-                                                        foreach($regiones as $region){
-                                                            $query->orwhere('suc.casid', $region);
-                                                        }
-
-                                                        foreach($zonas as $zona){
-                                                            $query->orwhere('suc.zonid', $zona);
-                                                        }
-
-                                                        foreach($grupos as $grupo){
-                                                            $query->orwhere('suc.gsuid', $grupo);
+                                                            $query->orwhere('fecano', $anio)
+                                                                ->where(function ($query) use($meses) {
+                                                                    foreach($meses as $mes){
+                                                                        $query->where('fecmes', $mes);
+                                                                    }
+                                                                })
+                                                                ->where(function ($query) use($regiones) {
+                                                                    foreach($regiones as $region){
+                                                                        $query->where('suc.casid', $region);
+                                                                    }
+                                                                })
+                                                                ->where(function ($query) use($zonas) {
+                                                                    foreach($zonas as $zona){
+                                                                        $query->where('suc.zonid', $zona);
+                                                                    }
+                                                                })
+                                                                ->where(function ($query) use($grupos) {
+                                                                    foreach($grupos as $grupo){
+                                                                        $query->orwhere('suc.gsuid', $grupo);
+                                                                    }
+                                                                })
+                                                                ->where('catid', $cat->catid)
+                                                                ->where('tsu.tprid', $tprid);
                                                         }
 
                                                     })
