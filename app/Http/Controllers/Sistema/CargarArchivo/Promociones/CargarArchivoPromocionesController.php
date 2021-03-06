b98dd93fa67f3c92e243eb94a248d0985c4c92c1
gersongrowanalytics <gerson.vilca@grow-analytics.com>
2021-03-06 13:07:18 -0500

    as

--- app/Http/Controllers/Sistema/CargarArchivo/Promociones/CargarArchivoPromocionesController.php
+++ app/Http/Controllers/Sistema/CargarArchivo/Promociones/CargarArchivoPromocionesController.php
@@ -4,6 +4,33 @@ namespace App\Http\Controllers\Sistema\CargarArchivo\Promociones;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Http\Request;
+use App\Http\Controllers\AuditoriaController;
+// use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+
+
+use Illuminate\Support\Str;
+use App\fecfechas;
+use App\tputiposusuarios;
+use App\perpersonas;
+use App\usuusuarios;
+use App\catcategorias;
+use App\proproductos;
+use App\tprtipospromociones;
+use App\cancanales;
+use App\ussusuariossucursales;
+use App\sucsucursales;
+use App\scasucursalescategorias;
+use App\csccanalessucursalescategorias;
+use App\prmpromociones;
+use App\cspcanalessucursalespromociones;
+use App\prbpromocionesbonificaciones;
+use App\prppromocionesproductos;
+use App\carcargasarchivos;
+use App\tuptiposusuariospermisos;
 
 class CargarArchivoPromocionesController extends Controller
 {
