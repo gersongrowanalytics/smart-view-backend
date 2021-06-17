a7d1c0587b4b339700425941c1fea02e40121a9c
gersongrowanalytics <gerson.vilca@grow-analytics.com>
2021-06-17 11:42:14 -0500

    generar json para descargar excel

--- routes/web.php
+++ routes/web.php
@@ -66,7 +66,7 @@ $router->group(['middleware' => ['permisos']], function() use($router) {
     $router->post('/promociones/descargar', 'Sistema\Promociones\Mostrar\CategoriasPromocionesMostrarController@mostrarCategoriasPromocionesExcel');
     $router->post('/promociones/descargar/especificos', 'Sistema\Promociones\Mostrar\CategoriasPromocionesMostrarController@MostrarSucursalesDescargarPromocionesExcel');
 
-    $router->post('/promociones/descargar/reporte-pagos', 'Sistema\Promociones\Mostrar\ReportePagos\MostrarReportePagosController@MostrarReportePagos');
+    $router->post('/promociones/descargar/reporte-pagos', 'Sistema\Promociones\ReportePagos\MostrarReportePagosController@MostrarReportePagos');
 
     $router->post('/promociones/editar', 'Sistema\Promociones\Editar\PromocionEditarController@editarPromocion');
     $router->post('/promociones/editar/imagenes', 'Sistema\Promociones\Editar\PromocionEditarImagenesController@EditarImagenesPromocion');
