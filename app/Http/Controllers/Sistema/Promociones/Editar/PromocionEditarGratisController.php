d110989069dec5b9bd3297766f20437d6b691aff
gersongrowanalytics <gerson.vilca@grow-analytics.com>
2021-09-08 10:03:36 -0500

    quitar gratis

--- app/Http/Controllers/Sistema/Promociones/Editar/PromocionEditarGratisController.php
+++ app/Http/Controllers/Sistema/Promociones/Editar/PromocionEditarGratisController.php
@@ -54,7 +54,26 @@ class PromocionEditarGratisController extends Controller
         $csps = cspcanalessucursalespromociones::join('prmpromociones as prm', 'prm.prmid', 'cspcanalessucursalespromociones.prmid')
                                                 ->join('prbpromocionesbonificaciones as prb', 'prb.prmid', 'prm.prmid')
                                                 ->where('cspcanalessucursalespromociones.fecid', $fecid)
-                                                ->where('prbcomprappt' , 'LIKE', 'Dscto.')
+                                                ->where('prbcomprappt' , 'LIKE', '%Dscto%')
+                                                ->get([
+                                                    'cspcanalessucursalespromociones.cspid',
+                                                    'cspgratis'
+                                                ]);
+
+        foreach($csps as $csp){
+            $cspe = cspcanalessucursalespromociones::find($csp->cspid);
+            $cspe->cspgratis = 0;
+            if($cspe->update()){
+                $logs["CSP_EDITADO"][] = $cspe->cspid;
+            }else{
+                $logs["CSP_NO_EDITADO"][] = $cspe->cspid;
+            }
+        }
+
+        $csps = cspcanalessucursalespromociones::join('prmpromociones as prm', 'prm.prmid', 'cspcanalessucursalespromociones.prmid')
+                                                ->join('prbpromocionesbonificaciones as prb', 'prb.prmid', 'prm.prmid')
+                                                ->where('cspcanalessucursalespromociones.fecid', $fecid)
+                                                ->where('prbcomprappt' , 'LIKE', 'Descuento')
                                                 ->get([
                                                     'cspcanalessucursalespromociones.cspid',
                                                     'cspgratis'
