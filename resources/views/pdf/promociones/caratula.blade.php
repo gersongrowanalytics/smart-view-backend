50105df52f67fd7e23cd478114c1b4c14fda7aa5
gersongrowanalytics <gerson.vilca@grow-analytics.com>
2021-12-07 17:51:55 -0500

    generar padf

--- resources/views/pdf/promociones/caratula.blade.php
+++ resources/views/pdf/promociones/caratula.blade.php
@@ -1,3 +1,3 @@
 <img class="body_page" src='../public/images/pdf/portada-14.jpg' alt="">
 <div class="box1_page1"> Catálogo de Promociones</div>
-<div class="box2_page1">Diciembre 2021</div>
+<div class="box2_page1">Noviembre 2021</div>
