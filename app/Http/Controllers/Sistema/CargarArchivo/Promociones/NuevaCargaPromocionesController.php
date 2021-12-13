90f93e5fcffa0f9852df899540390a626672ef21
gersongrowanalytics <gerson.vilca@grow-analytics.com>
2021-12-13 12:29:14 -0500

    nueva carga

--- app/Http/Controllers/Sistema/CargarArchivo/Promociones/NuevaCargaPromocionesController.php
+++ app/Http/Controllers/Sistema/CargarArchivo/Promociones/NuevaCargaPromocionesController.php
@@ -5,6 +5,36 @@ namespace App\Http\Controllers\Sistema\CargarArchivo\Promociones;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Http\Controllers\AuditoriaController;
+// use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+
+
+use Illuminate\Support\Str;
+use App\zonzonas;
+use App\fecfechas;
+use App\tputiposusuarios;
+use App\perpersonas;
+use App\usuusuarios;
+use App\catcategorias;
+use App\proproductos;
+use App\tprtipospromociones;
+use App\cancanales;
+use App\ussusuariossucursales;
+use App\sucsucursales;
+use App\scasucursalescategorias;
+use App\csccanalessucursalescategorias;
+use App\prmpromociones;
+use App\cspcanalessucursalespromociones;
+use App\prbpromocionesbonificaciones;
+use App\prppromocionesproductos;
+use App\carcargasarchivos;
+use App\tuptiposusuariospermisos;
+use \DateTime;
+use PhpOffice\PhpSpreadsheet\Shared\Date;
+
 class NuevaCargaPromocionesController extends Controller
 {
     public function NuevaCargaPromociones(Request $request)
