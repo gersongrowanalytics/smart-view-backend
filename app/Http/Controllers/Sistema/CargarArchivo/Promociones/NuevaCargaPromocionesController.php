282329f5cfb75d60622f8e0514f523793754d1e5
gersongrowanalytics <gerson.vilca@grow-analytics.com>
2021-12-13 12:26:22 -0500

    nueva carga

--- app/Http/Controllers/Sistema/CargarArchivo/Promociones/NuevaCargaPromocionesController.php
+++ app/Http/Controllers/Sistema/CargarArchivo/Promociones/NuevaCargaPromocionesController.php
@@ -1,42 +1,13 @@
-<?php //Promociones Set 2021.xlsx
+<?php
 
 namespace App\Http\Controllers\Sistema\CargarArchivo\Promociones;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Http\Controllers\AuditoriaController;
-// use PhpOffice\PhpSpreadsheet\Spreadsheet;
-use PhpOffice\PhpSpreadsheet\IOFactory;
-use PhpOffice\PhpSpreadsheet\Spreadsheet;
-use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
-
-use Illuminate\Support\Str;
-use App\zonzonas;
-use App\fecfechas;
-use App\tputiposusuarios;
-use App\perpersonas;
-use App\usuusuarios;
-use App\catcategorias;
-use App\proproductos;
-use App\tprtipospromociones;
-use App\cancanales;
-use App\ussusuariossucursales;
-use App\sucsucursales;
-use App\scasucursalescategorias;
-use App\csccanalessucursalescategorias;
-use App\prmpromociones;
-use App\cspcanalessucursalespromociones;
-use App\prbpromocionesbonificaciones;
-use App\prppromocionesproductos;
-use App\carcargasarchivos;
-use App\tuptiposusuariospermisos;
-use \DateTime;
-use PhpOffice\PhpSpreadsheet\Shared\Date;
-
-class CargarArchivoPromocionesController extends Controller
+class NuevaCargaPromocionesController extends Controller
 {
-    public function CargarArchivos(Request $request)
+    public function NuevaCargaPromociones(Request $request)
     {
 
         $respuesta      = true;
@@ -977,6 +948,4 @@ class CargarArchivoPromocionesController extends Controller
         return $requestsalida;
 
     }
-
-    
 }
