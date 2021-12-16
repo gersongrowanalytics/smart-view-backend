44d13c1e6d441407b8e6e5850e6786ac28a45068
gersongrowanalytics <gerson.vilca@grow-analytics.com>
2021-12-16 14:39:59 -0500

    todos a3 volteado  y sin linea separadora

--- resources/views/pdf/promociones/titulocategoria.blade.php
+++ resources/views/pdf/promociones/titulocategoria.blade.php
@@ -1,33 +1,33 @@
 
 
 @if ($data['catid'] == 1)
-<img class="body_page" src='../public/images/pdf/Azul-portada-16.jpg' alt="">
-<img class="img_body_page" src="../public/images/pdf/family.trazo-39.jpg" alt="">
+<img class="body_page" src='../public/images/pdf/portadas/portadafamily.jpg' alt="">
+<!-- <img class="img_body_page" src="../public/images/pdf/family.trazo-39.jpg" alt=""> -->
 @endif
 
 @if ($data['catid'] == 2)
-<img class="body_page" src='../public/images/pdf/Rojo-portada-16.jpg' alt="">
-<img class="img_body_page" src="../public/images/pdf/infant.trazo-39.jpg" alt="">
+<img class="body_page" src='../public/images/pdf/portadas/portadainfant.jpg' alt="">
+<!-- <img class="img_body_page" src="../public/images/pdf/infant.trazo-39.jpg" alt=""> -->
 @endif
 
 @if ($data['catid'] == 3)
-<img class="body_page" src='../public/images/pdf/Lila-portada-16.jpg' alt="">
-<img class="img_body_page" src="../public/images/pdf/Adult.trazo-39.jpg" alt="">
+<img class="body_page" src='../public/images/pdf/portadas/portadaadulto.jpg' alt="">
+<!-- <img class="img_body_page" src="../public/images/pdf/Adult.trazo-39.jpg" alt=""> -->
 @endif
 
 @if ($data['catid'] == 4)
-<img class="body_page" src='../public/images/pdf/Verde-portada-16.jpg' alt="">
-<img class="img_body_page" src="../public/images/pdf/wipes.trazo-39.jpg" alt="">
+<img class="body_page" src='../public/images/pdf/portadas/portadawipes.jpg' alt="">
+<!-- <img class="img_body_page" src="../public/images/pdf/wipes.trazo-39.jpg" alt=""> -->
 @endif
 
 @if ($data['catid'] == 5)
-<img class="body_page" src='../public/images/pdf/Morado-portada-16.jpg' alt="">
-<img class="img_body_page" src="../public/images/pdf/fem.trazo-39.jpg" alt="">
+<img class="body_page" src='../public/images/pdf/portadas/portadafem.jpg' alt="">
+<!-- <img class="img_body_page" src="../public/images/pdf/fem.trazo-39.jpg" alt=""> -->
 @endif
 
 @if ($data['catid'] == 6)
-<img class="body_page" src='../public/images/pdf/Celeste-portada-16.jpg' alt="">
-<img class="img_body_page" src="../public/images/pdf/multicategoria.trazo-39.jpg" alt="">
+<img class="body_page" src='../public/images/pdf/portadas/portadamulticategoria.jpg' alt="">
+<!-- <img class="img_body_page" src="../public/images/pdf/multicategoria.trazo-39.jpg" alt=""> -->
 @endif
 
 
