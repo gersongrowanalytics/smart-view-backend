c9c52922e5483cb1e8dae741e8728f2f66d102e9
gersongrowanalytics <gerson.vilca@grow-analytics.com>
2021-12-17 02:05:31 -0500

    todos a3 volteado  y sin linea separadora

--- resources/views/pdf/promociones/indicecategorias.blade.php
+++ resources/views/pdf/promociones/indicecategorias.blade.php
@@ -1,2 +1,19 @@
 <img class="body_page" src='../public/images/pdf/Indice.jpg' alt="">
 <div class="Texto-Titulo-Indice-Promociones" >Índice</div>
+<div class="Contenedor-Tarjetas-Categorias-Indice-Pdf">
+    @foreach($categorias as $categoria)
+        @if($categoria['catid'] == 1)
+        <img class="Icono-Indice-Promociones-Pdf" src='../public/images/pdf/iconoindice/indicefamily.jpg' alt=""><br>
+        @elseif($categoria['catid'] == 2)
+        <img class="Icono-Indice-Promociones-Pdf" src='../public/images/pdf/iconoindice/indiceinfant.jpg' alt=""><br>
+        @elseif($categoria['catid'] == 3)
+        <img class="Icono-Indice-Promociones-Pdf" src='../public/images/pdf/iconoindice/indiceadult.jpg' alt=""><br>
+        @elseif($categoria['catid'] == 4)
+        <img class="Icono-Indice-Promociones-Pdf" src='../public/images/pdf/iconoindice/indicewipes.jpg' alt="">
+        @elseif($categoria['catid'] == 5)
+        <img class="Icono-Indice-Promociones-Pdf" src='../public/images/pdf/iconoindice/indicefem.jpg' alt=""><br>
+        @elseif($categoria['catid'] == 6)
+        <img class="Icono-Indice-Promociones-Pdf" src='../public/images/pdf/iconoindice/indicemulticategoria.jpg' alt=""><br>
+        @endif
+    @endforeach
+</div>
