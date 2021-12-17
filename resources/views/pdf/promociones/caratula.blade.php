fc84a747dd3fa81a34bfa96ea2e0302ac37e34b8
gersongrowanalytics <gerson.vilca@grow-analytics.com>
2021-12-17 14:33:26 -0500

    s

--- resources/views/pdf/promociones/caratula.blade.php
+++ resources/views/pdf/promociones/caratula.blade.php
@@ -1,3 +1,6 @@
 <img class="body_page" src='../public/images/pdf/portada.jpg' alt="">
 <!-- <div class="titulo_caratula_pdf"> <b>Catálogo de Promociones</b></div> -->
-<div class="subtitulo_caratula_pdf">{{$titulocaratula}}</div>
+<div class="Contenedor-Todo-Pdf">
+    <div class="subtitulo_caratula_pdf">{{$titulocaratula}}</div>
+</div>
+<!-- <div class="subtitulo_caratula_pdf">{{$titulocaratula}}</div> -->
