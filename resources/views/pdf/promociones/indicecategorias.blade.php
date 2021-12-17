3d7a4a30bcabbd3ef6b5c1648c9055b6d075ea47
gersongrowanalytics <gerson.vilca@grow-analytics.com>
2021-12-17 02:10:30 -0500

    todos a3 volteado  y sin linea separadora

--- resources/views/pdf/promociones/indicecategorias.blade.php
+++ resources/views/pdf/promociones/indicecategorias.blade.php
@@ -2,17 +2,17 @@
 <!-- <div class="Texto-Titulo-Indice-Promociones" >Índice</div> -->
 <div class="Contenedor-Tarjetas-Categorias-Indice-Pdf">
     @foreach($categorias as $categoria)
-        @if($categoria['catid'] == 1)
+        @if($categoria['catid'] == 1 && sizeof($categoria['canales']) > 0)
         <img class="Icono-Indice-Promociones-Pdf" src='../public/images/pdf/iconoindice/indicefamily.jpg' alt=""><br>
-        @elseif($categoria['catid'] == 2)
+        @elseif($categoria['catid'] == 2 && sizeof($categoria['canales']) > 0)
         <img class="Icono-Indice-Promociones-Pdf" src='../public/images/pdf/iconoindice/indiceinfant.jpg' alt=""><br>
-        @elseif($categoria['catid'] == 3)
+        @elseif($categoria['catid'] == 3 && sizeof($categoria['canales']) > 0)
         <img class="Icono-Indice-Promociones-Pdf" src='../public/images/pdf/iconoindice/indiceadult.jpg' alt=""><br>
-        @elseif($categoria['catid'] == 4)
+        @elseif($categoria['catid'] == 4 && sizeof($categoria['canales']) > 0)
         <img class="Icono-Indice-Promociones-Pdf" src='../public/images/pdf/iconoindice/indicewipes.jpg' alt="">
-        @elseif($categoria['catid'] == 5)
+        @elseif($categoria['catid'] == 5 && sizeof($categoria['canales']) > 0)
         <img class="Icono-Indice-Promociones-Pdf" src='../public/images/pdf/iconoindice/indicefem.jpg' alt=""><br>
-        @elseif($categoria['catid'] == 6)
+        @elseif($categoria['catid'] == 6 && sizeof($categoria['canales']) > 0)
         <img class="Icono-Indice-Promociones-Pdf" src='../public/images/pdf/iconoindice/indicemulticategoria.jpg' alt=""><br>
         @endif
     @endforeach
