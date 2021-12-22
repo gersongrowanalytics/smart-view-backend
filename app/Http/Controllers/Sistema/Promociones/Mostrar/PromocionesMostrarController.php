d38e1c5170bd8c3880f5a7de1dc885458153a823
gersongrowanalytics <gerson.vilca@grow-analytics.com>
2021-12-22 18:29:43 -0500

    ordenar

--- app/Http/Controllers/Sistema/Promociones/Mostrar/PromocionesMostrarController.php
+++ app/Http/Controllers/Sistema/Promociones/Mostrar/PromocionesMostrarController.php
@@ -987,9 +987,9 @@ class PromocionesMostrarController extends Controller
         usort(
             $cscs,
             function ($a, $b)  {
-                if ($a['cantidadPromociones'] > $b['cantidadPromociones']) {
+                if ($a->cantidadPromociones > $b->cantidadPromociones) {
                     return -1;
-                } else if ($a['cantidadPromociones'] < $b['cantidadPromociones']) {
+                } else if ($a->cantidadPromociones < $b->cantidadPromociones) {
                     return 1;
                 } else {
                     return 0;
