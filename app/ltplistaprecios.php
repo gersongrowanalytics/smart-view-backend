2bdc1caff40a403258492acca0fe637c8e0b6e76
gersongrowanalytics <gerson.vilca@grow-analytics.com>
2022-01-20 16:56:20 -0500

    ruta para agrega la lista de precios

---
+++ app/ltplistaprecios.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ltplistaprecios extends Model
+{
+    
+    protected $table = 'ltplistaprecios';
+    protected $primaryKey = 'ltpid';
+
+}
