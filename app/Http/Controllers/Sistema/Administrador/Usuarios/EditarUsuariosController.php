5b49b0abb18028d165b327354787126e79703042
gersongrowanalytics <gerson.vilca@grow-analytics.com>
2022-03-21 12:19:12 -0500

    controladores para crear editar y mostrar usuarios en administrador

---
+++ app/Http/Controllers/Sistema/Administrador/Usuarios/EditarUsuariosController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Sistema\Administrador\Usuarios;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class EditarUsuariosController extends Controller
+{
+    //
+}
