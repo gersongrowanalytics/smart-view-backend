50aad09a77caeaf53ee6af85d467ab4b7f4d06f6
gersongrowanalytics <gerson.vilca@grow-analytics.com>
2022-03-22 15:13:37 -0500

    crear varios rebate

--- app/Http/Controllers/Sistema/Modulos/Rebate/Crear/CrearRebateController.php
+++ app/Http/Controllers/Sistema/Modulos/Rebate/Crear/CrearRebateController.php
@@ -40,39 +40,40 @@ class CrearRebateController extends Controller
                 foreach($re_data['data'] as $data){
 
                     if(isset($data['editando'])){
-                        if($data['editando'] == true){
-                    
-                            $rtp = rtprebatetipospromociones::where('fecid', $fec->fecid)
-                                                            ->where('tprid', $data['tprideditando'])
-                                                            ->where('rtpporcentajedesde', $data['desdeeditando'])
-                                                            ->where('rtpporcentajehasta', $data['hastaeditando'])
-                                                            ->where('rtpporcentajerebate', $data['rebateeditando'])
-                                                            ->first();
-                            
-                            $rtpid = 0;
-        
-                            if($rtp){
-                                $rtpid = $rtp->rtpid;
-                            }else{
-                                $rtpn = new rtprebatetipospromociones;
-                                $rtpn->fecid = $fec->fecid;
-                                $rtpn->tprid = $data['tprideditando'];
-                                $rtpn->rtpporcentajedesde  = $data['desdeeditando'];
-                                $rtpn->rtpporcentajehasta  = $data['hastaeditando'];
-                                $rtpn->rtpporcentajerebate = $data['rebateeditando'];
-                                if($rtpn->save()){
-                                    $rtpid = $rtpn->rtpid;
-                                }else{
-                                    $respuesta = false;
-                                    $mensaje = "Lo sentimos algunos rebates no fueron agregados";
-                                }
-                            }
+                        if($data['editando'] == true){ 
         
                             foreach($cats as $cat){
+
+                                $rtp = rtprebatetipospromociones::where('fecid', $fec->fecid)
+                                                                ->where('tprid', $data['tprideditando'])
+                                                                ->where('rtpporcentajedesde', $data['desdeeditando'])
+                                                                ->where('rtpporcentajehasta', $data['hastaeditando'])
+                                                                ->where('rtpporcentajerebate', $data['cat-'.$cat->catid])
+                                                                ->first();
+                                
+                                $rtpid = 0;
+            
+                                if($rtp){
+                                    $rtpid = $rtp->rtpid;
+                                }else{
+                                    $rtpn = new rtprebatetipospromociones;
+                                    $rtpn->fecid = $fec->fecid;
+                                    $rtpn->tprid = $data['tprideditando'];
+                                    $rtpn->rtpporcentajedesde  = $data['desdeeditando'];
+                                    $rtpn->rtpporcentajehasta  = $data['hastaeditando'];
+                                    $rtpn->rtpporcentajerebate = $data['cat-'.$cat->catid];
+                                    if($rtpn->save()){
+                                        $rtpid = $rtpn->rtpid;
+                                    }else{
+                                        $respuesta = false;
+                                        $mensaje = "Lo sentimos algunos rebates no fueron agregados";
+                                    }
+                                }
+
         
                                 $trr = trrtiposrebatesrebates::where('treid', $data['treideditando'] )
                                                             ->where('rtpid', $rtpid)
-                                                            ->where('catid', $data['cat-'.$cat->catid])
+                                                            ->where('catid', $cat->catid)
                                                             ->first();
         
                                 if($trr){
@@ -81,7 +82,7 @@ class CrearRebateController extends Controller
                                     $trrn = new trrtiposrebatesrebates;
                                     $trrn->treid = $data['treideditando'];
                                     $trrn->rtpid = $rtpid;
-                                    $trrn->catid = $data['cat-'.$cat->catid];
+                                    $trrn->catid = $cat->catid;
                                     if($trrn->save()){
         
                                     }else{
