368484914c4b1ed5d601d6218e4a05816f3ffbcf
gerson.vilca@grow-analytics.com <gerson.vilca@grow-analytics.com>
2022-03-30 19:23:55 -0500

    commit

---
+++ app/tnotiposnotificaciones.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class tnotiposnotificaciones extends Model
+{
+    protected $table = 'tnotiposnotificaciones';
+    protected $primaryKey = 'tnoid';
+}
