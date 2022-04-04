2bfa7ffdd27deb00c47469a93238d09d902c8e3b
gerson.vilca@grow-analytics.com <gerson.vilca@grow-analytics.com>
2022-04-03 23:01:12 -0500

    mostrar todas las cat de sca exceptuando el 6

--- app/Http/Controllers/Sistema/Ventas/Mostrar/VentasMostrarController.php
+++ app/Http/Controllers/Sistema/Ventas/Mostrar/VentasMostrarController.php
@@ -235,7 +235,8 @@ class VentasMostrarController extends Controller
 
                     $scasucursalescategorias = scasucursalescategorias::join('catcategorias as cat', 'cat.catid', 'scasucursalescategorias.catid')
                                                                     ->where('scasucursalescategorias.tsuid', $tsutipopromocionsucursal->tsuid)
-                                                                    ->where('cat.catid', '<', 6)
+                                                                    // ->where('cat.catid', '<', 6)
+                                                                    ->where('cat.catid', '!=', 6)
                                                                     ->orderBy('cat.catid')
                                                                     ->get([
                                                                         'cat.catnombre',
