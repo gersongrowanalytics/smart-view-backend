2fe09f2f2e690a5abc8f59e1449a438e2839e54a
gersongrowanalytics <gerson.vilca@grow-analytics.com>
2022-04-07 12:22:11 -0500

    asignar sku

---
+++ app/impimagenesproductos.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class impimagenesproductos extends Model
+{
+    protected $table = 'impimagenesproductos';
+    protected $primaryKey = 'impid';
+}
