4e6bbb49732e3eccf23b0b1ff38b4801517f3e03
gersongrowanalytics <gerson.vilca@grow-analytics.com>
2022-04-17 23:18:18 -0500

    mostrar ebate

--- app/Http/Controllers/Sistema/Ventas/Mostrar/MostrarRebateTrismestralController.php
+++ app/Http/Controllers/Sistema/Ventas/Mostrar/MostrarRebateTrismestralController.php
@@ -41,114 +41,86 @@ class MostrarRebateTrismestralController extends Controller
                                 'tpr.tprnombre',
                             ]);
 
-            foreach($ttrs as $posicionTtr => $ttr){
+            if(sizeof($ttrs) > 0){
 
-                if($posicionTtr == 0){
-                    $ttrs[$posicionTtr]['mostrando'] = true;
-                }else{
-                    $ttrs[$posicionTtr]['mostrando'] = false;
-                }
+                foreach($ttrs as $posicionTtr => $ttr){
 
-                $ttrs[$posicionTtr]['ocultando'] = false;
-                $ttrs[$posicionTtr]['retroceder'] = false;
-
-                $ttrs[$posicionTtr]['data'] = array();
-                $arr_datas = array();
-
-                $ttrsPorcentajes = ttrtritre::join('tretiposrebates as tre', 'tre.treid', 'ttrtritre.treid')
-                            ->where('ttrtritre.triid', $trf->triid)
-                            ->where('tre.treid', $ttr->treid)
-                            ->distinct('ttrporcentajedesde')
-                            ->distinct('ttrporcentajehasta')
-                            ->get([
-                                'ttrporcentajedesde',
-                                'ttrporcentajehasta'
-                            ]);
-
-                foreach($ttrsPorcentajes as $ttrsPorcentaje){
-                    $arr_data = array(
-                        'tprid'  => $ttr->tprid,
-                        'tprnombre'  => $ttr->tprnombre,
-                        'trenombre'  => $ttr->trenombre,
-                        'ttrporcentajedesde'  => $ttrsPorcentaje->ttrporcentajedesde,
-                        'ttrporcentajehasta'  => $ttrsPorcentaje->ttrporcentajehasta,
-                        'ttrporcentajerebate' => "1",
-                    );
-
-                    $ttrscats = ttrtritre::join('tretiposrebates as tre', 'tre.treid', 'ttrtritre.treid')
-                                        ->where('ttrtritre.triid', $trf->triid)
-                                        ->where('tre.treid', $ttr->treid)
-                                        ->where('ttrporcentajedesde', $ttrsPorcentaje->ttrporcentajedesde)
-                                        ->where('ttrporcentajehasta', $ttrsPorcentaje->ttrporcentajehasta)
-                                        ->get([
-                                            'catid',
-                                            'ttrporcentajerebate'
-                                        ]);
-
-
-                    foreach($cats as $cat){
-                        
-                        $porcentajeTtr = 0;
-
-                        foreach($ttrscats as $ttrscat){
-                            
-                            if($ttrscat->catid == $cat->catid){
-                                $porcentajeTtr = $ttrscat->ttrporcentajerebate;
-                            }
-
-                        }
-
-                        $arr_data['cat-'.$cat->catid] = $porcentajeTtr;
-
+                    if($posicionTtr == 0){
+                        $ttrs[$posicionTtr]['mostrando'] = true;
+                    }else{
+                        $ttrs[$posicionTtr]['mostrando'] = false;
                     }
-
-                    $arr_datas[] = $arr_data;
+    
+                    $ttrs[$posicionTtr]['ocultando'] = false;
+                    $ttrs[$posicionTtr]['retroceder'] = false;
+    
+                    $ttrs[$posicionTtr]['data'] = array();
+                    $arr_datas = array();
+    
+                    $ttrsPorcentajes = ttrtritre::join('tretiposrebates as tre', 'tre.treid', 'ttrtritre.treid')
+                                ->where('ttrtritre.triid', $trf->triid)
+                                ->where('tre.treid', $ttr->treid)
+                                ->distinct('ttrporcentajedesde')
+                                ->distinct('ttrporcentajehasta')
+                                ->get([
+                                    'ttrporcentajedesde',
+                                    'ttrporcentajehasta'
+                                ]);
+    
+                    foreach($ttrsPorcentajes as $ttrsPorcentaje){
+                        $arr_data = array(
+                            'tprid'  => $ttr->tprid,
+                            'tprnombre'  => $ttr->tprnombre,
+                            'trenombre'  => $ttr->trenombre,
+                            'ttrporcentajedesde'  => $ttrsPorcentaje->ttrporcentajedesde,
+                            'ttrporcentajehasta'  => $ttrsPorcentaje->ttrporcentajehasta,
+                            'ttrporcentajerebate' => "1",
+                        );
+    
+                        $ttrscats = ttrtritre::join('tretiposrebates as tre', 'tre.treid', 'ttrtritre.treid')
+                                            ->where('ttrtritre.triid', $trf->triid)
+                                            ->where('tre.treid', $ttr->treid)
+                                            ->where('ttrporcentajedesde', $ttrsPorcentaje->ttrporcentajedesde)
+                                            ->where('ttrporcentajehasta', $ttrsPorcentaje->ttrporcentajehasta)
+                                            ->get([
+                                                'catid',
+                                                'ttrporcentajerebate'
+                                            ]);
+    
+    
+                        foreach($cats as $cat){
+                            
+                            $porcentajeTtr = 0;
+    
+                            foreach($ttrscats as $ttrscat){
+                                
+                                if($ttrscat->catid == $cat->catid){
+                                    $porcentajeTtr = $ttrscat->ttrporcentajerebate;
+                                }
+    
+                            }
+    
+                            $arr_data['cat-'.$cat->catid] = $porcentajeTtr;
+    
+                        }
+    
+                        $arr_datas[] = $arr_data;
+                    }
+    
+                    $ttrs[$posicionTtr]['data'] = $arr_datas;
+    
+    
                 }
+    
+                $data = $ttrs;
 
-                $ttrs[$posicionTtr]['data'] = $arr_datas;
-
-
+            }else{
+                $data = $this->ObtenerDataVacia();
             }
-
-            $data = $ttrs;
 
         }else{
 
-            $data = array();
-
-            $tre = tretiposrebates::where('trenombre', 'ZB')
-                                    ->first();
-
-            if($tre){
-
-                $data[] = array(
-                    "treid"      => $tre->treid,
-                    "trenombre"  => $tre->trenombre,
-                    "mostrando"  => true,
-                    "ocultando"  => false,
-                    "retroceder" => false,
-                    "tprid"      => 1,
-                    "tprnombre"  => "Sell In",
-                    "data" => array(
-                        array(
-                            "cat-1" => "1",
-                            "cat-2" => "1",
-                            "cat-3" => "1",
-                            "cat-4" => "1",
-                            "cat-5" => "1",
-                            "cat-6" => 0,
-                            "cat-7" => 0,
-                            "tprid" => 1,
-                            "tprnombre" => "Sell In",
-                            "trenombre" => "ZB",
-                            "ttrporcentajedesde"  => "100",
-                            "ttrporcentajehasta"  => "104",
-                            "ttrporcentajerebate" => "1",
-                        )
-                    )
-                );
-
-            }
+            $data = $this->ObtenerDataVacia();
 
             
 
@@ -159,5 +131,46 @@ class MostrarRebateTrismestralController extends Controller
             "datos"     => $data
         ]);
 
+    }
+
+    public function ObtenerDataVacia()
+    {
+        $data = array();
+
+        $tre = tretiposrebates::where('trenombre', 'ZB')
+                                ->first();
+
+        if($tre){
+
+            $data[] = array(
+                "treid"      => $tre->treid,
+                "trenombre"  => $tre->trenombre,
+                "mostrando"  => true,
+                "ocultando"  => false,
+                "retroceder" => false,
+                "tprid"      => 1,
+                "tprnombre"  => "Sell In",
+                "data" => array(
+                    array(
+                        "cat-1" => "1",
+                        "cat-2" => "1",
+                        "cat-3" => "1",
+                        "cat-4" => "1",
+                        "cat-5" => "1",
+                        "cat-6" => 0,
+                        "cat-7" => 0,
+                        "tprid" => 1,
+                        "tprnombre" => "Sell In",
+                        "trenombre" => "ZB",
+                        "ttrporcentajedesde"  => "100",
+                        "ttrporcentajehasta"  => "104",
+                        "ttrporcentajerebate" => "1",
+                    )
+                )
+            );
+
+        }
+
+        return $data;
     }
 }
