6c62e417074c138f1e4ab730fa47170daa99a524
unknown <Marco>
2022-04-19 16:23:37 -0500

    modificacion de vista promociones nuevas

--- resources/views/Promociones/PromocionesNuevas.blade.php
+++ resources/views/Promociones/PromocionesNuevas.blade.php
@@ -6,44 +6,82 @@
         <style type="text/css">
             @import url(https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap);
         </style>
-        <div style="width: 100%">
+
+        <div style="width: 100%; ">
             <center>
-                <div style="width: 482px">
-                    <div style="height: 45px; background: #FEDD34; padding-top: 17px">
-                        <div style="font-style: normal; font-weight: 700; font-size: 20px; line-height: 27px; color: #1E1E1E;">
-                            Creciendo Juntos
-                        </div>
-                    </div>
-                    <div style="padding-top: 32px; padding-left: 67px; padding-right: 66px">
-                        <div style="font-style: normal; font-weight: 700; font-size: 13px; line-height: 17px; letter-spacing: -0.015em; color: #1E1E1E;">
-                            <!-- Estimado cliente NORDISAC, -->
-                            Estimado cliente {{$txtSucursales}},
-                        </div>
-                        <div style="font-style: normal; font-weight: 400; font-size: 13px; line-height: 17px; letter-spacing: -0.015em; color: #1E1E1E; margin-top: 12px">
-                            Por el presente correo, cumplo con informarle que cuenta con <span style="font-weight: 700;">Nuevas Promociones</span> para este mes de
-                            <span style="font-weight: 700;"> {{$re_fecha}}</span>
-                        </div>
-                        <a href="https://leadsmartview.com/sistema/promociones">
-                            <div style="padding-top: 10px; margin-top: 17px; background: #FEDD34; border-radius: 22px; width: 175px; height: 25px; font-weight: 700; font-size: 13px; line-height: 17px; color: #1E1E1E;">
-                                <center>Visitar Plataforma</center>
+                <table border="0" cellspacing="0" cellpadding="0" style="width: 482px">
+                    <tr>
+                        <td style="width: 100%;">
+                            <div style="width: 100%">
+                                <table style="background: #FEDD34; width: 100%">
+                                    <tr style="height: 45px;">
+                                        <td align="center">
+                                            <div style="font-style: normal; font-weight: 700; font-size: 20px; line-height: 27px; color: #1E1E1E;">
+                                                Creciendo Juntos
+                                            </div>
+                                        </td>
+                                    </tr>
+                                </table>
+                                <table border="0" cellspacing="0" cellpadding="0" style="padding-top: 32px; padding-left: 67px; padding-right: 66px">
+                                    <tr>
+                                        <td colspan="3" align="center">
+                                            <div style="font-style: normal; font-weight: 700; font-size: 13px; line-height: 17px; letter-spacing: -0.015em; color: #1E1E1E;">
+                                                <!-- Estimado cliente NORDISAC, -->
+                                                Estimado cliente {{$txtSucursales}},
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    <tr style="padding-top: 5px;">
+                                        <td colspan="3" align="center">
+                                            <div style="font-style: normal; font-weight: 400; font-size: 13px; line-height: 17px; letter-spacing: -0.015em; color: #1E1E1E; margin-top: 12px">
+                                                Por el presente correo, cumplo con informarle que cuenta con <span style="font-weight: 700;">Nuevas Promociones</span> para este mes de
+                                                <span style="font-weight: 700;"> {{$re_fecha}}</span>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    <tr style="padding-top: 5px;">
+                                        <td></td>
+                                        <td align="center" style="width: 100%">
+                                            <a href="https://leadsmartview.com/sistema/promociones">
+                                                <div style="text-align:center; padding-top: 10px; margin-top: 17px; background: #FEDD34; border-radius: 22px; width: 175px; height: 25px; font-weight: 700; font-size: 13px; line-height: 17px; color: #1E1E1E;">
+                                                    Visitar Plataforma
+                                                </div>
+                                            </a>
+                                        </td>
+                                        <td></td>
+                                    </tr>
+                                    <tr style="padding-top: 23px;">
+                                        <td colspan="3" align="center">
+                                            <div style="padding-top: 23px; font-style: normal; font-weight: 400; font-size: 11px; line-height: 15px; color: #1E1E1E;">
+                                                En caso tengas problemas para ingresar en el botón, ingresar al siguiente enlace: <a href="https://leadsmartview.com/sistema/promociones">https://leadsmartview.com/sistema/promociones</a>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    <tr style="padding-top: 15px;">
+                                        <td colspan="3" align="justify">
+                                            <div style="padding-top: 15px; font-style: italic; font-weight: 400; font-size: 12px; line-height: 16px; text-align: justify; letter-spacing: -0.015em; color: #000000">
+                                                Este es un email automático, si tienes cualquier tipo de duda ponte en contacto con nosotros a través de nuestro 
+                                                servicio de atención al cliente al Cuidatunegocio.kc@kcc.com, por favor no respondas a este mensaje.
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    <tr style="height: 17px">
+                                        <td></td>
+                                    </tr>
+                                </table>
+                                <table style="width: 100%; background: #F1F1F1;">
+                                    <tr style="height: 57px;">
+                                        <td align="center">
+                                            <div style="font-style: normal; font-weight: bold; font-size: 9px; line-height: 11px; color: black;">
+                                                © 2021 GROW ANALYTICS
+                                            </div>
+                                        </td>
+                                    </tr>
+                                </table>
                             </div>
-                        </a>
-                        <div style="padding-top: 23px; font-style: normal; font-weight: 400; font-size: 11px; line-height: 15px; color: #1E1E1E;">
-                            En caso tengas problemas para ingresar en el botón, ingresar al siguiente enlace: <a href="https://leadsmartview.com/sistema/promociones">https://leadsmartview.com/sistema/promociones</a>
-                        </div>
-                        <div style="padding-top: 15px; font-style: italic; font-weight: 400; font-size: 12px; line-height: 16px; text-align: justify; letter-spacing: -0.015em; color: #000000">
-                            Este es un email automático, si tienes cualquier tipo de duda ponte en contacto con nosotros a través de nuestro 
-                            servicio de atención al cliente al Cuidatunegocio.kc@kcc.com, por favor no respondas a este mensaje.
-                        </div>
-                    </div>
-                    <div style="background: #F1F1F1; margin-top:30px; height: 57px;">
-                        <center>
-                            <div style=" font-style: normal; font-weight: bold; font-size: 9px; line-height: 11px; color: black; padding-top:25px">
-                                © 2021 GROW ANALYTICS
-                            </div>
-                        </center>
-                    </div>
-                </div>
+                        </td>
+                    </tr>
+                </table>
             </center>
         </div>
     </body>
