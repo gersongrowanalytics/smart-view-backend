57d286da5bf2feb6f218166649911f7392be919c
gersongrowanalytics <gerson.vilca@grow-analytics.com>
2022-04-22 22:18:10 -0500

    mostrar tipos

--- app/Http/Controllers/Sistema/ElementrosEnviados/MostrarTiposElementosEnviadosController.php
+++ app/Http/Controllers/Sistema/ElementrosEnviados/MostrarTiposElementosEnviadosController.php
@@ -14,7 +14,7 @@ class MostrarTiposElementosEnviadosController extends Controller
         $respuesta = false;
         $mensaje = "";
 
-        $tno = uceusuarioscorreosenviados::distinct('ucetipo')->get();
+        $tno = uceusuarioscorreosenviados::distinct('ucetipo');
 
         if (sizeof($tno) > 0) {
             $respuesta      = true;
