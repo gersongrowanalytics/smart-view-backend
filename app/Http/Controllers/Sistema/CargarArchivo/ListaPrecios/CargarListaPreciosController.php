375e6bebcf1941ec9a06403f947d01a36a0f79b9
gersongrowanalytics <gerson.vilca@grow-analytics.com>
2022-04-26 10:36:19 -0500

    cambiar fecha

--- app/Http/Controllers/Sistema/CargarArchivo/ListaPrecios/CargarListaPreciosController.php
+++ app/Http/Controllers/Sistema/CargarArchivo/ListaPrecios/CargarListaPreciosController.php
@@ -47,7 +47,7 @@ class CargarListaPreciosController extends Controller
         $exitoSubirExcel = false;
 
         $nombres = [];
-        $fechaSeleccionada = 153;
+        $fechaSeleccionada = 71;
         $gruposEncontrados = [];
 
         DB::beginTransaction();
