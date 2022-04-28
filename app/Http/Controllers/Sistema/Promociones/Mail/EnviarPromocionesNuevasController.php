dc93a0e9ec8f03708589533e2858847b0b16ac4d
gersongrowanalytics <gerson.vilca@grow-analytics.com>
2022-04-28 18:05:19 -0500

    mail

--- app/Http/Controllers/Sistema/Promociones/Mail/EnviarPromocionesNuevasController.php
+++ app/Http/Controllers/Sistema/Promociones/Mail/EnviarPromocionesNuevasController.php
@@ -20,7 +20,7 @@ class EnviarPromocionesNuevasController extends Controller
 
         $usutoken   = $request->header('api_token');
         // $correo = "gerson.vilca@grow-analytics.com.pe";
-        $correo = "tkm_tkm@hotmail.com";
+        $correo = "euni_tkm@hotmail.com";
         // $correo = "director.creativo@grow-analytics.com.pe";
         // $correo = "jeanmarcoe@gmail.com";
 
