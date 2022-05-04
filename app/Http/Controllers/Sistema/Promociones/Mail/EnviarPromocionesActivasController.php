23d6dbe08d1426e69bc01607ba9b40ec08a7a38f
gerson.vilca@grow-analytics.com <gerson.vilca@grow-analytics.com>
2022-05-04 11:47:18 -0500

    re

--- app/Http/Controllers/Sistema/Promociones/Mail/EnviarPromocionesActivasController.php
+++ app/Http/Controllers/Sistema/Promociones/Mail/EnviarPromocionesActivasController.php
@@ -29,8 +29,8 @@ class EnviarPromocionesActivasController extends Controller
         // $correo = "eunicecallecahuana@gmail.com";
         // $correo = "jeanmarcoe@gmail.com";
 
-        // $correo = "gerson.vilca@grow-analytics.com.pe";
-        $correo = "director.creativo@grow-analytics.com.pe";
+        $correo = "gerson.vilca@grow-analytics.com.pe";
+        // $correo = "director.creativo@grow-analytics.com.pe";
         // $correo = "miguel.caballero@grow-analytics.com.pe";
         // $correo = "mzorrilla@kcc.com";
 
