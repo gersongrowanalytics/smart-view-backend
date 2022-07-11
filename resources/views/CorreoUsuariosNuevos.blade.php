70a2b94313966735eb4a5b84892e086c2520c7c0
Marco <marco.espinoza@grow-analytics.com.pe>
2022-07-11 18:53:57 -0500

    vista para enviar correo de usuarios nuevos

---
+++ resources/views/CorreoUsuariosNuevos.blade.php
@@ -0,0 +1,132 @@
+<html>
+<head>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet">
+</head>
+<body>
+    <style type="text/css">
+        @import url(https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap);
+    </style>
+    <div style="width: 100%; ">
+        <center>
+            <div style="font-style: normal; font-weight: normal; font-size: 12px; line-height: 12px; color: #4d4d4d; ">
+                <span>¡Hola Glose!</span>
+                <div style="margin-top: 5px">Agradecemos tu confianza e interés en conocer nuestra plataforma, y deseamos que utilizando <span style="font-weight: bold">Creciendo</br></span> </div> 
+                <div style="margin-top: 5px"><span style="font-weight: bold">Juntos </span> la visibilidad deseada de tus <span style="font-weight: bold">Promociones</span></br></div>
+                <div style="margin-top: 5px">Desde ya queremos que sepas que estamos comprometidos en darles el mejor servicio posible, por lo cual para </br></div>
+                <div style="margin-top: 5px">empezar queremos entregarte información de interés.</div>
+            </div>
+            <div style="width: 440px;">
+                <div id="piesCorreo" style="background: #558CFF; margin-top:30px; height: 45px; width: 440px; padding-top: 0px; position:relative">
+                    <table border="0" cellspacing="0" cellpadding="0" style="width: 100%; position:absolute;" >
+                        <tr style="width: 100%;">
+                            <td style="width: 12.5%; height:5px; background: #558CFF"></td>
+                            <td style="width: 12.5%; height:5px; background: #4157BD"></td>
+                            <td style="width: 12.5%; height:5px; background: #4761D1"></td>
+                            <td style="width: 12.5%; height:5px; background: #4B68E2"></td>
+                            <td style="width: 12.5%; height:5px; background: #558CFF"></td>
+                            <td style="width: 12.5%; height:5px; background: #4157BD"></td>
+                            <td style="width: 12.5%; height:5px; background: #4761D1"></td>
+                            <td style="width: 12.5%; height:5px; background: #4B68E2"></td>
+                        </tr>
+                    </table>
+                    <div 
+                        id="" 
+                        style=" font-style: normal; font-weight: 900; font-size: 18px; line-height: 21px; color: #FFFFFF; padding-top:15px;">
+                        Creciendo Juntos
+                    </div>
+                </div>
+                <table border="0" cellspacing="0" cellpadding="0" style="width: 100%"> 
+                    <tr>
+                        <td colspan="8" style="background: #edf0f3; height: 15px;"></td>
+                    </tr>
+                    <tr>
+                        <td style="background: #edf0f3; width: 30px"></td>
+                        <td colspan="6"  align="center" >
+                            <div id="" style=" font-style: normal; font-weight: bold; font-size: 15px; line-height: 18px; color: #558CFF; padding-top:15px">
+                                Hola, Gloser
+                            </div>
+                        </td>
+                        <td style="background: #edf0f3; width: 30px"></td>
+                    </tr>
+                    <tr >
+                        <td style="background: #edf0f3; width: 30px"></td>
+                        <td colspan="6"  align="center">
+                            <div style="margin-left: 60px; margin-right: 60px; margin-bottom:15px; margin-top:5px">
+                                <span style=" font-style: normal; font-weight: bold; font-size: 12px; line-height: 12px; color: #4d4d4d; ">
+                                    ¡Se creó con éxito tu cuenta!
+                                </span><br/>
+                            </div>
+                        </td>
+                        <td style="background: #edf0f3; width: 30px"></td>
+                    </tr>
+                    <tr>
+                        <td style="background: #edf0f3; width: 30px"></td>
+                        <td colspan="6" align="center" >
+                            <div style="margin-bottom:15px">
+                                <span style=" font-style: normal; font-weight: normal; font-size: 13px; line-height: 12px; color: #9c9b9b;">
+                                    Para iniciar sesión debes ingresar con el siguiente </br> usuario y contraseña 
+                                </span>
+                            </div>
+                        </td>
+                        <td style="background: #edf0f3; width: 30px"></td>
+                    </tr>
+                    <tr>
+                        <td style="background: #edf0f3; width: 30px"></td>
+                        <td colspan="6" align="center">
+                            <div style="margin-left: 10px; margin-right: 10px; font-style: normal; font-weight: normal; font-size: 12px; line-height: 12px; color: #9c9b9b;">
+                                Usuario
+                            </div>
+                        </td>
+                        <td style="background: #edf0f3; width: 30px"></td>
+                    </tr>
+                    <tr>
+                        <td style="background: #edf0f3; width: 30px"></td>
+                        <td colspan="6" align="center">
+                            <div style="margin-left: 10px; margin-right: 10px; margin-top: 5px; font-style: normal; font-weight: normal; font-size: 12px; line-height: 12px; color: #4d4d4d">
+                                {{$usuario}}
+                            </div>
+                        </td>
+                        <td style="background: #edf0f3; width: 30px"></td>
+                    </tr>
+                    <tr>
+                        <td style="background: #edf0f3; width: 30px"></td>
+                        <td colspan="6" align="center">
+                            <div style="margin-left: 10px; margin-right: 10px; margin-top: 10px; font-style: normal; font-weight: normal; font-size: 12px; line-height: 12px; color: #9c9b9b;">
+                                Contraseña
+                            </div>
+                        </td>
+                        <td style="background: #edf0f3; width: 30px"></td>
+                    </tr>
+                    <tr>
+                        <td style="background: #edf0f3; width: 30px"></td>
+                        <td colspan="6" align="center">
+                            <div style="margin-left: 10px; margin-right: 10px; margin-top: 5px; margin-bottom: 30px; font-style: normal; font-weight: normal; font-size: 12px; line-height: 12px; color: #4d4d4d">
+                                {{$contrasenia}}
+                            </div>
+                        </td>
+                        <td style="background: #edf0f3; width: 30px"></td>
+                    </tr>
+                    <tr>
+                        <td style="background: #edf0f3; width: 30px"></td>
+                        <td colspan="6" align="center">
+                            <div style="font-size: 10px; margin-bottom: 45px">
+                                <a 
+                                    id="btnPlataforma" 
+                                    href="http://leadsmartview.com/"
+                                    style="text-decoration: none; color: white; background: #558CFF; padding-top: 10px; padding-bottom: 10px; padding-left: 35px; padding-right: 35px; border-radius: 22px; margin-top: 50px; font-style: normal; font-weight: bold; font-size: 10px; line-height: 12px;"
+                                >IR A PLATAFORMA</a>
+                            </div>
+                        </td>
+                        <td style="background: #edf0f3; width: 30px;"></td>
+                    </tr>
+                    <tr>
+                        <td colspan="8" style="background: #edf0f3; height: 25px;"></td>
+                    </tr>
+                </table>
+            </div>
+        </center>
+        
+    </div>
+
+</body>
+</html>
