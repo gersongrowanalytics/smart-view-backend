0ec2fbe952d13c61512aaca46b615721d91e2560
Gerson <gerson.vilca@grow-analytics.com.pe>
2022-08-22 16:24:01 -0500

    registrar movivimeitnos

--- app/Http/Controllers/Sistema/CargarArchivo/MetRegistrarMovimientoStatusController.php
+++ app/Http/Controllers/Sistema/CargarArchivo/MetRegistrarMovimientoStatusController.php
@@ -11,29 +11,29 @@ class MetRegistrarMovimientoStatusController extends Controller
 {
     public function MetRegistrarMovimientoStatus ($badid, $carid)
     {
-        // $respuesta = false;
+        $respuesta = false;
 
-        // $meses = ["ENE", "FEB", "MAR", "ABR", "MAY", "JUN", "JUL", "AGO", "SET", "OCT", "NOV", "DIC"];
-        // date_default_timezone_set("America/Lima");
-        // $anioActual = date('Y');
-        // $mesActual  = date('n');
-        // $mesActualTexto = $meses[$mesActual - 1];
+        $meses = ["ENE", "FEB", "MAR", "ABR", "MAY", "JUN", "JUL", "AGO", "SET", "OCT", "NOV", "DIC"];
+        date_default_timezone_set("America/Lima");
+        $anioActual = date('Y');
+        $mesActual  = date('n');
+        $mesActualTexto = $meses[$mesActual - 1];
         
-        // $coa = coacontrolarchivos::join('fecfechas as fec', 'fec.fecid', 'coacontrolarchivos.fecid')
-        //                         ->where('badid', $badid)
-        //                         ->where('fec.fecano', $anioActual)
-        //                         ->where('fec.fecmes', $mesActualTexto)
-        //                         ->first('coaid');
+        $coa = coacontrolarchivos::join('fecfechas as fec', 'fec.fecid', 'coacontrolarchivos.fecid')
+                                ->where('badid', $badid)
+                                ->where('fec.fecano', $anioActual)
+                                ->where('fec.fecmes', $mesActualTexto)
+                                ->first('coaid');
 
-        // if ($coa) {
-        //     $coa->carid = $carid;
-        //     $coa->estid = 3;
-        //     if ($coa->update()) {
-        //         $respuesta = true;
-        //     }
-        // }
+        if ($coa) {
+            $coa->carid = $carid;
+            $coa->estid = 3;
+            if ($coa->update()) {
+                $respuesta = true;
+            }
+        }
              
-        $respuesta = true;
+        // $respuesta = true;
         return $respuesta;
     }
 }
