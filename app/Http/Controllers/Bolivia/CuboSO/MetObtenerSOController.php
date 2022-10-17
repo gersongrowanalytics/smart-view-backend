4748354ee171f12045bb798890f003bc4a89dfd1
Gerson <gerson.vilca@grow-analytics.com.pe>
2022-10-17 10:45:50 -0500

    cubo so

--- app/Http/Controllers/Bolivia/CuboSO/MetObtenerSOController.php
+++ app/Http/Controllers/Bolivia/CuboSO/MetObtenerSOController.php
@@ -5,15 +5,22 @@ namespace App\Http\Controllers\Bolivia\CuboSO;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\spider_view_1;
+use Illuminate\Support\Facades\DB;
 
 class MetObtenerSOController extends Controller
 {
     public function ObtenerSO(Request $request)
     {
 
-        $spider = spider_view_1::limit(10);
+        // $spider = spider_view_1::limit(10);
 
-        return $spider;
+        // return $spider;
+
+        $tables = DB::select('SHOW TABLES');
+        foreach($tables as $table)
+        {
+            echo $table->Tables_in_db_name;
+        }
 
     }
 }
