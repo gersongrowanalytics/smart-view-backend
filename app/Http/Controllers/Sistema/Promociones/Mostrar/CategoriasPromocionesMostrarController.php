774b0439443ae2eb84d7bf342dc4a9695e8f5208
Gerson <gerson.vilca@grow-analytics.com.pe>
2022-12-02 09:17:44 -0500

    download promociones

--- app/Http/Controllers/Sistema/Promociones/Mostrar/CategoriasPromocionesMostrarController.php
+++ app/Http/Controllers/Sistema/Promociones/Mostrar/CategoriasPromocionesMostrarController.php
@@ -661,6 +661,7 @@ class CategoriasPromocionesMostrarController extends Controller
                 
                 $csps = cspcanalessucursalespromociones::leftjoin('prmpromociones as prm', 'prm.prmid', 'cspcanalessucursalespromociones.prmid')
                                                         ->leftjoin('proproductos as pro', 'pro.prosku', 'prm.prmsku')
+                                                        ->leftjoin('proproductos as probonif', 'pro.prosku', 'prm.prmskubonificado')
                                                         ->leftjoin('csccanalessucursalescategorias as csc', 'csc.cscid', 'cspcanalessucursalespromociones.cscid')
                                                         ->leftjoin('cancanales as can', 'can.canid', 'csc.canid')
                                                         ->leftjoin('scasucursalescategorias as sca', 'sca.scaid', 'csc.scaid')
@@ -689,7 +690,9 @@ class CategoriasPromocionesMostrarController extends Controller
                                                             'cannombre',
                                                             'catnombre',
                                                             'prm.prmsku',
-                                                            'pronombre',
+                                                            'pro.pronombre as pronombre',
+                                                            'prm.prmskubonificado',
+                                                            'probonif.pronombre as pronombrebonif',
                                                             'prmmecanica',
                                                             'cspcantidadcombo',
                                                             'cspcantidadplancha',
@@ -902,10 +905,48 @@ class CategoriasPromocionesMostrarController extends Controller
                                         )
                                     );
 
+                                }else if($re_columna['columna'] == "Sku Bonificado"){
+
+                                    $arrayTitulos[] = array(
+                                        "title" => "Sku Bonificado",
+                                        "style" => array(
+                                            "fill" => array(
+                                                "patternType" => "solid",
+                                                "fgColor" => array(
+                                                    "rgb" => $colorAzul
+                                                )
+                                            ),
+                                            "font" => array(
+                                                "color" => array(
+                                                    "rgb" => $colorBlanco
+                                                )
+                                            )
+                                        )
+                                    );
+
                                 }else if($re_columna['columna'] == "Producto"){
 
                                     $arrayTitulos[] = array(
                                         "title" => "Producto",
+                                        "style" => array(
+                                            "fill" => array(
+                                                "patternType" => "solid",
+                                                "fgColor" => array(
+                                                    "rgb" => $colorAzul
+                                                )
+                                            ),
+                                            "font" => array(
+                                                "color" => array(
+                                                    "rgb" => $colorBlanco
+                                                )
+                                            )
+                                        )
+                                    );
+
+                                }else if($re_columna['columna'] == "Producto Bonificado"){
+
+                                    $arrayTitulos[] = array(
+                                        "title" => "Producto Bonificado",
                                         "style" => array(
                                             "fill" => array(
                                                 "patternType" => "solid",
@@ -1202,7 +1243,39 @@ class CategoriasPromocionesMostrarController extends Controller
                                     )
                                 ),
                                 array(
+                                    "title" => "Sku Bonificado",
+                                    "style" => array(
+                                        "fill" => array(
+                                            "patternType" => "solid",
+                                            "fgColor" => array(
+                                                "rgb" => $colorAzul
+                                            )
+                                        ),
+                                        "font" => array(
+                                            "color" => array(
+                                                "rgb" => $colorBlanco
+                                            )
+                                        )
+                                    )
+                                ),
+                                array(
                                     "title" => "Producto",
+                                    "style" => array(
+                                        "fill" => array(
+                                            "patternType" => "solid",
+                                            "fgColor" => array(
+                                                "rgb" => $colorAzul
+                                            )
+                                        ),
+                                        "font" => array(
+                                            "color" => array(
+                                                "rgb" => $colorBlanco
+                                            )
+                                        )
+                                    )
+                                ),
+                                array(
+                                    "title" => "Producto Bonificado",
                                     "style" => array(
                                         "fill" => array(
                                             "patternType" => "solid",
@@ -1340,7 +1413,9 @@ class CategoriasPromocionesMostrarController extends Controller
                         $desc_cannombre = $csp->cannombre;
                         $desc_catnombre = $csp->catnombre;
                         $desc_prmsku    = $csp->prmsku;
+                        $desc_prmsku_bonif = $csp->prmskubonificado;
                         $desc_pronombre = $csp->pronombre;
+                        $desc_pronombre_bonif = $csp->pronombrebonif;
                         $desc_prmmecanica = $csp->prmmecanica;
 
                         $desc_cspcantidadcombo   = $csp->cspcantidadcombo;
@@ -1376,8 +1451,14 @@ class CategoriasPromocionesMostrarController extends Controller
                         if($desc_prmsku == null || $desc_prmsku == " " || $desc_prmsku == "-" ){
                             $desc_prmsku = "0";
                         }
+                        if($desc_prmsku_bonif == null || $desc_prmsku_bonif == " " || $desc_prmsku_bonif == "-" ){
+                            $desc_prmsku_bonif = "0";
+                        }
                         if($desc_pronombre == null || $desc_pronombre == " " || $desc_pronombre == "-" ){
                             $desc_pronombre = "0";
+                        }
+                        if($desc_pronombre_bonif == null || $desc_pronombre_bonif == " " || $desc_pronombre_bonif == "-" ){
+                            $desc_pronombre_bonif = "0";
                         }
                         if($desc_prmmecanica == null || $desc_prmmecanica == " " || $desc_prmmecanica == "-" ){
                             $desc_prmmecanica = "0";
@@ -1544,10 +1625,32 @@ class CategoriasPromocionesMostrarController extends Controller
                                         )
                                     );
 
+                                }else if($re_columna['columna'] == "Sku Bonificado"){
+
+                                    $arrayFilaExcel[] = array(
+                                        "value" => $desc_prmsku_bonif,
+                                        "style" => array(
+                                            "font" => array(
+                                                "sz" => "9"
+                                            )
+                                        )
+                                    );
+
                                 }else if($re_columna['columna'] == "Producto"){
 
                                     $arrayFilaExcel[] = array(
                                         "value" => $desc_pronombre,
+                                        "style" => array(
+                                            "font" => array(
+                                                "sz" => "9"
+                                            )
+                                        )
+                                    );
+
+                                }else if($re_columna['columna'] == "Producto Bonificado"){
+
+                                    $arrayFilaExcel[] = array(
+                                        "value" => $desc_pronombre_bonif,
                                         "style" => array(
                                             "font" => array(
                                                 "sz" => "9"
