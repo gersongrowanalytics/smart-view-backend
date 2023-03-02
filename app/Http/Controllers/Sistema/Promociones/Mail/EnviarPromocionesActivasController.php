27167ad04f4302f9bfc38f9b18ab69b985b33335
Gerson <gerson.vilca@grow-analytics.com.pe>
2023-03-02 18:36:15 -0500

    enviar correos

--- app/Http/Controllers/Sistema/Promociones/Mail/EnviarPromocionesActivasController.php
+++ app/Http/Controllers/Sistema/Promociones/Mail/EnviarPromocionesActivasController.php
@@ -474,9 +474,6 @@ class EnviarPromocionesActivasController extends Controller
                     // Mail::to($usuarios_correo)->cc(['0540Peru.salescontrolling@kcc.com', 'Cuidatunegocio.KC@kcc.com', 'gerson.vilca@grow-analytics.com.pe', 'miguel.caballero@grow-analytics.com.pe', 'eunice.calle@grow-analytics.com.pe'])
                     //                             ->send(new MailPromocionesActivas($data_correo, $asunto_correo));
 
-                    Mail::to($usuarios_correo)->cc(['0540Peru.salescontrolling@kcc.com', 'Cuidatunegocio.KC@kcc.com', 'gerson.vilca@grow-analytics.com.pe', 'eunice.calle@grow-analytics.com.pe'])
-                                                ->send(new MailPromocionesActivas($data_correo, $asunto_correo));
-
                     // ENVIAR RECORDATORIO
                     // Mail::to($usuarios_correo)->cc(['eunice.calle@grow-analytics.com.pe'])
                     //                             ->send(new MailPromocionesActivas($data_correo, $asunto_correo));
