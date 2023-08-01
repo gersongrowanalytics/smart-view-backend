2f99899f9f1d0ef8129adbaeee7e9b317a688241
Gerson <gerson.vilca@grow-analytics.com.pe>
2023-07-31 19:39:12 -0500

    where periodo

--- app/Http/Controllers/Sistema/SellOut/ObtenerSellOutController.php
+++ app/Http/Controllers/Sistema/SellOut/ObtenerSellOutController.php
@@ -43,6 +43,7 @@ class ObtenerSellOutController extends Controller
                                     ->where('SALES', '>', 0)
                                     ->distinct('PK_CLIENT_SO')
                                     ->orderBy('PK_CLIENT_SO')
+                                    ->groupby('PK_CLIENT_SO')
                                     ->count();
 
         $so = PortafolioSales::where('PERIOD', '=', '202212')
@@ -50,6 +51,7 @@ class ObtenerSellOutController extends Controller
                                 ->where('SALES', '>', 0)
                                 ->distinct('PK_CLIENT_SO')
                                 ->orderBy('PK_CLIENT_SO')
+                                ->groupby('PK_CLIENT_SO')
                                 ->limit(100)
                                 ->get();
 
